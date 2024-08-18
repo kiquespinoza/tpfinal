753a4a1db41ef2bc63adbc4aa35a073109394a82
ENRIQUE ESPINOZA CERDA <EESPICE@bci.cl>
2024-08-17 22:32:30 -0400

    carga de csv

---
+++ tpfinal.py
@@ -0,0 +1,10 @@
+import pandas as pd  
+
+
+df1 = pd.read_csv(r'C:\Users\eespice\Desktop\tp2\pds\ecommerce_customers_dataset.csv')
+df2 = pd.read_csv(r'C:\Users\eespice\Desktop\tp2\pds\ecommerce_order_items_dataset.csv')
+df3 = pd.read_csv(r'C:\Users\eespice\Desktop\tp2\tp2.0\ecommerce_order_payments_dataset.csv')
+df4 = pd.read_csv(r'C:\Users\eespice\Desktop\tp2\pds\ecommerce_orders_dataset.csv')
+df5 = pd.read_csv(r'C:\Users\eespice\Desktop\tp2\pds\ecommerce_products_dataset.csv')
+
+
